0fc649dc8d2a09d3de2a63abc17b7fa2ebe58162
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-11 17:20:11 +0530

    monthly payment

---
+++ Payment/Payment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Payment
+{
+     class Util
+    {
+        public void MonthlyPayemt(int P,int y,int R)
+        {
+            double n, r;
+            //double p = P;
+            
+            n = 12 * y;  
+            r = R / (12 * 100);
+            
+            double monthpay = P * ( r / (1 - Math.Pow((1 + r), -n)));
+            Console.WriteLine("the payment is {0}", monthpay); 
+        } 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the principle amount");
+            int P = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter no. of  years ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter rate of interest");
+            int R = Convert.ToInt32(Console.ReadLine());
+
+            Util u = new Util();
+            u.MonthlyPayemt(P, y, R);
+
+            //Util.MonthlyPayemt(P, y, R);
+        }
+    }
+}
