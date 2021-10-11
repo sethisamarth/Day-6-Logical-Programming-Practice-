263c27e8d49104612e2ea37aa11bafcaf944c8da
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-11 15:20:09 +0530

    Celsius to Fahrenheit

---
+++ TemperatureConverter/TemperatureConverter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    class Util
+    {
+        public void Fah(double C)
+        {
+            double F;
+            F = (C * 1.8) + 32;
+            Console.WriteLine("temperature in Fahrenheit:{0}", F);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            Console.WriteLine("Enter temperature in deg C to get in Fahrenheit ");
+            double C = Convert.ToDouble(Console.ReadLine());
+            Util u = new Util();
+            u.Fah(C);
+        }
+    }
+}
