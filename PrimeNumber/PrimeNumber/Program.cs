522615813551f373c433c1f9dbfc54984da19e9a
Samarth sethi <90337565+sethisamarth@users.noreply.github.com>
2021-10-11 09:53:27 +0530

    Update Program.cs

--- PrimeNumber/PrimeNumber/Program.cs
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -9,7 +9,7 @@ namespace PrimeNumber
             int i; int flag = 1;
             Console.WriteLine("Enter a number to check its Prime or not ");
             int n = Convert.ToInt32(Console.ReadLine());
-            for (i = 2; i < n; i++)
+            for (i = 2; i < n/2; i++)
             {
                 if (n % i == 0)
                 {
