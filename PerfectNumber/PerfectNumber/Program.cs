6d2cc9a921f94d2377de1f4f159fa3f4b9a400eb
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-11 10:41:51 +0530

    Checking its Perfect number or not

---
+++ PerfectNumber/PerfectNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the value of n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 1, sum1 = 0, finalsum = 0;
+            for (i = 1; i < n * n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                    sum1 = sum1 + i;
+                }
+
+            }
+            finalsum = sum1 - n;
+            Console.WriteLine("value of final sum ={0}",finalsum);  
+            if (finalsum == n)
+            {
+                Console.WriteLine("its a perfect no.");
+            }
+            else
+            {
+                Console.WriteLine("its a not  perfect no.");
+
+            }
+
+        }
+    }
+}
